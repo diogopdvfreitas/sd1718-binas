c99ad58de6c7ddc3d7f3cf32e14570e061a1457c
jominutaro <jominutaro@hotmail.com>
2018-04-12 15:28:16 +0100

    GetMethodTest, closes #9

---
+++ binas-ws-cli/src/test/java/org/binas/ws/it/GetCreditTest.java
@@ -0,0 +1,55 @@
+package org.binas.ws.it;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.binas.ws.EmailExists_Exception;
+import org.binas.ws.InvalidEmail_Exception;
+import org.binas.ws.UserNotExists_Exception;
+import org.binas.ws.UserView;
+
+public class GetCreditTest extends BaseIT{
+
+	private static final Integer USER_INITIAL_POINTS = 10;
+	private static final String EMAIL = "e.2.mail@that.serv1ce.com";
+	private static final String EMAIL1 = "e.2.mailil@that.serv1ce.com";
+	
+	@Before
+	public void setUp() throws Exception {
+		client.testInit(USER_INITIAL_POINTS);
+	}
+	
+	@Test
+	public void sucess() {
+		try {
+			UserView userView = client.activateUser(EMAIL);
+			int credit = client.getCredit(EMAIL);
+			assertEquals(userView.getCredit().intValue(), credit);	
+		} catch (UserNotExists_Exception uee) {
+			fail("User does not exists");
+		} catch (EmailExists_Exception eee) {
+			fail("Invalid duplicated user error");
+		} catch (InvalidEmail_Exception iee) {
+			fail("Invalid email format error");
+		}
+	}
+	
+	@Test(expected = UserNotExists_Exception.class)
+	public void userExists() throws UserNotExists_Exception {
+		try {
+			client.activateUser(EMAIL);
+			client.getCredit(EMAIL1);
+		} catch (EmailExists_Exception eee) {
+			fail("Invalid duplicated user error");
+		} catch (InvalidEmail_Exception iee) {
+			fail("Invalid email format error");
+		}
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		client.testClear();
+	}
+}
