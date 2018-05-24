fe284b477749f706cf42ddb4485ffccd4fd7d155
fchamicapereira <fchamicapereira@gmail.com>
2018-05-24 23:10:36 +0100

    demonstration

--- binas-ws-cli/src/main/java/org/binas/ws/cli/BinasClientApp.java
+++ binas-ws-cli/src/main/java/org/binas/ws/cli/BinasClientApp.java
@@ -1,5 +1,11 @@
 package org.binas.ws.cli;
 
+import java.util.List;
+import java.util.Scanner;
+
+import org.binas.ws.CoordinatesView;
+import org.binas.ws.StationView;
+
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINamingException;
 
 public class BinasClientApp {
@@ -47,8 +53,25 @@ public class BinasClientApp {
 		 
 		
 		try {
-			client.activateUser("e@email.com");
-			System.out.print("My credit=" + client.getCredit("e@email.com"));			
+			String email = "e@email.com";
+			client.activateUser(email);
+			
+			CoordinatesView cv = new CoordinatesView();
+			cv.setX(0);
+			cv.setY(0);
+			
+			List<StationView> stations = client.listStations(1, cv);
+			
+			client.rentBina(stations.get(0).getId(), email);
+			System.out.println("My credit=" + client.getCredit(email));
+			
+			System.out.println("Input waiting...");
+			Scanner reader = new Scanner(System.in);
+			reader.next();
+			System.out.println("Proceeding");
+			
+			System.out.println("My credit=" + client.getCredit(email));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
